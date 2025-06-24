d4ae7586333b303cca186b0e36813e5edb815413
Etai Nadler <etainad008@gmail.com>
2025-06-24 17:07:32 +0300

    add scram script

---
+++ addSASLScramUser.sh
@@ -0,0 +1,5 @@
+docker exec -it kafka-controller-0 kafka-storage format \                           
+  --config /etc/kafka/kraft/server.properties \
+  --cluster-id Rv_mOiSXQMSkcOpL_jZ01Q \
+  --add-scram 'SCRAM-SHA-512=[name=ninja,password=hi]' \
+  --ignore-formatted
